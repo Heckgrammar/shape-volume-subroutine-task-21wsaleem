801d3612b61287def5c4d57bde2f1140bed3a4f0
21wsaleem <144434650+21wsaleem@users.noreply.github.com>
2025-04-01 16:59:41 +0100

    Update Program.cs
    new

--- ShapeAreaCalculatorTask/Program.cs
+++ ShapeAreaCalculatorTask/Program.cs
@@ -1,19 +1,29 @@
-﻿namespace ShapeAreaCalculatorTask
+using System;
+
+namespace ShapeAreaCalculatorTask
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //Write subroutines to calculate the areas of 
-            // Circle
-            // Triangle
-            // Rectangle
-            // Pentagon
-            // Octagon
+            Console.WriteLine("Rectangle Area: " + AreaOfRectangle(7, 4));
+            Console.WriteLine("Circle Area: " + AreaOfCircle(5));
+            Console.WriteLine("Triangle Area: " + AreaOfTriangle(10, 8));
         }
-        static int areaOfRectangle()//Add the parameters
-        {
 
+        static double AreaOfRectangle(double length, double width)
+        {
+            return length * width;
+        }
+
+        static double AreaOfCircle(double radius)
+        {
+            return 3.14 * radius * radius;
+        }
+
+        static double AreaOfTriangle(double b, double h)
+        {
+            return 0.5 * b * h;
         }
     }
 }
